fdfc690b46fb4a3bbbc5dc38712649ad699e958f
Ayush Malakar <ayush.ethical@gmail.com>
2026-02-06 23:26:13 +0530

    Some changes is made like link

--- src/main/java/com/example/smart/billing/config/SecurityConfig.java
+++ src/main/java/com/example/smart/billing/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://smart-store-billing.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
